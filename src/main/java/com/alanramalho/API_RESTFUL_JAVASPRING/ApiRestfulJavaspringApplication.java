f9873288a29041c27ace9b67117864770a9c43ec
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-16 15:37:03 -0300

    Primeiro commit- Projeto criado

---
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/ApiRestfulJavaspringApplication.java
@@ -0,0 +1,15 @@
+package com.alanramalho.API_RESTFUL_JAVASPRING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulJavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulJavaspringApplication.class, args);
+
+		System.out.println("Aplicação iniciada com sucesso.");
+	}
+
+}
---
+++ src/test/java/com/alanramalho/API_RESTFUL_JAVASPRING/ApiRestfulJavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanramalho.API_RESTFUL_JAVASPRING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulJavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
