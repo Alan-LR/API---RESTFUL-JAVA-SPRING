87aa41959adb89f60344abcfa409abc955b1b565
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-18 18:49:32 -0300

    inserção de usuário com o post

--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/resources/UserResource.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/resources/UserResource.java
@@ -1,16 +1,22 @@
 package com.alanramalho.API_RESTFUL_JAVASPRING.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import javax.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alanramalho.API_RESTFUL_JAVASPRING.domain.User;
 import com.alanramalho.API_RESTFUL_JAVASPRING.dto.UserDTO;
@@ -43,6 +49,14 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = userService.fromDTO(objDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/services/UserService.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alanramalho.API_RESTFUL_JAVASPRING.domain.User;
+import com.alanramalho.API_RESTFUL_JAVASPRING.dto.UserDTO;
 import com.alanramalho.API_RESTFUL_JAVASPRING.repository.UserRepository;
 import com.alanramalho.API_RESTFUL_JAVASPRING.services.ServicesExceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRep.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRep.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail(), objDto.getSenha());
     }
 }
