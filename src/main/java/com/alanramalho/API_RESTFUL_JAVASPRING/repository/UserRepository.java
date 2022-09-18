416bae3d9ac71366938df11c298afba6cff7f3eb
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-18 20:10:17 -0300

    Atualização de usuário com put

--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/repository/UserRepository.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.alanramalho.API_RESTFUL_JAVASPRING.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/resources/UserResource.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,6 +65,15 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable String id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = userService.fromDTO(objDto);
+        obj.setId(id);
+        obj = userService.update(obj);
+        return ResponseEntity.noContent().build();
+
     }
 
 }
