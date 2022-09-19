f882173056e08efa6e1e9ed642ee007895bb0a84
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-19 10:23:12 -0300

    Criando entity post com user aninhado

--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/config/Instantiation.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.alanramalho.API_RESTFUL_JAVASPRING.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.alanramalho.API_RESTFUL_JAVASPRING.domain.Post;
 import com.alanramalho.API_RESTFUL_JAVASPRING.domain.User;
+import com.alanramalho.API_RESTFUL_JAVASPRING.repository.PostRepository;
 import com.alanramalho.API_RESTFUL_JAVASPRING.repository.UserRepository;
 
 @Configuration
@@ -15,14 +19,28 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRep;
 
+    @Autowired
+    private PostRepository postRep;
+
     @Override
     public void run(String... args) throws Exception {
 
-        // // userRep.deleteAll();
-        // User maria = new User(null, "Maria Brown", "maria@gmail.com", "ma2254");
-        // User alex = new User(null, "Alex Green", "alex@gmail.com", "ale899");
-        // User bob = new User(null, "Bob Grey", "bob@gmail.com", "bob33@");
-        // userRep.saveAll(Arrays.asList(maria, alex, bob));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRep.deleteAll();
+        postRep.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com", "ma2254");
+        User alex = new User(null, "Alex Green", "alex@gmail.com", "ale899");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com", "bob33@");
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços!",
+                maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje", maria);
+
+        userRep.saveAll(Arrays.asList(maria, alex, bob));
+        postRep.saveAll(Arrays.asList(post1, post2));
     }
 
 }
--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/repository/PostRepository.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.alanramalho.API_RESTFUL_JAVASPRING.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.alanramalho.API_RESTFUL_JAVASPRING.domain.User;
+import com.alanramalho.API_RESTFUL_JAVASPRING.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
