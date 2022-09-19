a11f2c4eb6f6ea1a87612766b82024585e04b4c0
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-19 16:08:39 -0300

    Consulta com varios criterios

--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/repository/PostRepository.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.alanramalho.API_RESTFUL_JAVASPRING.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -17,4 +18,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
     // vai buscar todos os posts, ignorando camel case
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    // Consulta com varios criterios - a:396
+    @Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } }, { $or: [ {'title': { $regex: ?0, $options: 'i'} }, {'body': { $regex: ?0, $options: 'i'} }, {'comments.text': { $regex: ?0, $options: 'i'} } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/resources/PostResource.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.alanramalho.API_RESTFUL_JAVASPRING.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,18 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> obj = postService.findByTitle(text);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> obj = postService.fullSearch(text, min, max);
         return ResponseEntity.ok().body(obj);
     }
 
