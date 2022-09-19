f44a27cf7dad6409eb8747c4730bbb19ed3b94a7
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-19 11:54:41 -0300

    Obtendo post por um id

---
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/services/PostService.java
@@ -0,0 +1,23 @@
+package com.alanramalho.API_RESTFUL_JAVASPRING.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanramalho.API_RESTFUL_JAVASPRING.domain.Post;
+import com.alanramalho.API_RESTFUL_JAVASPRING.repository.PostRepository;
+import com.alanramalho.API_RESTFUL_JAVASPRING.services.ServicesExceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRep;
+
+    public Post findById(String id) {
+        Optional<Post> obj = postRep.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
