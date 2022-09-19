3aee0580f57e84acf79d715afb5e75afe18ff1cc
Alan Ramalho <alanlopes.r@gmail.com>
2022-09-19 13:00:39 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/domain/Post.java
+++ src/main/java/com/alanramalho/API_RESTFUL_JAVASPRING/domain/Post.java
@@ -1,13 +1,15 @@
 package com.alanramalho.API_RESTFUL_JAVASPRING.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.alanramalho.API_RESTFUL_JAVASPRING.dto.AuthorDTO;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.alanramalho.API_RESTFUL_JAVASPRING.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +21,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
 
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
